72e7b74fb364229c22f360ef95051f93df7f0b93
Philippe <phil_2121@msn.com>
2016-05-29 18:15:45 -0400

    Programme final

---
+++ src/FileReaderBasic.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Robin on 2016-05-12.
+ */
+public class FileReaderBasic {
+
+    /** function used to save the dictionary needed for the execution of the algorithm */
+    public ArrayList<String> readDictionary(String fileName){
+
+        // array list of all the values contained in the dictionnary
+        ArrayList<String> lineListDict = createValueTable(fileName);
+
+        return lineListDict;
+    }
+
+    /** function used to save the lines of texts to be analysed */
+    public ArrayList<String> readLines(String fileName){
+        // array list of all the values to be analysed by the program
+        ArrayList<String> LineListFile = createValueTable(fileName);
+
+        return LineListFile;
+    }
+
+    /** create a table of values from a textfile */
+    private ArrayList<String> createValueTable (String filepath){
+        ArrayList<String> lineListDict;
+
+        try(BufferedReader newReader = new BufferedReader(new FileReader(filepath))){
+            String currentLine;
+
+            lineListDict = new ArrayList<String>();
+
+            // we fill the arrayList
+            while((currentLine = newReader.readLine()) != null)
+            {
+                lineListDict.add(currentLine);
+            }
+
+            newReader.close();
+            return lineListDict;
+        }
+        catch(IOException e){
+            // printing the error in the console
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /** get the path of the file to open */
+    private String getFile()
+    {
+
+        JFileChooser fileChooser = new JFileChooser();
+
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION)
+        {
+            File selectedFile = fileChooser.getSelectedFile();
+            return selectedFile.getPath();
+        }
+        else if(returnValue == JFileChooser.CANCEL_OPTION)
+        {
+            System.exit(0);
+        }
+
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,105 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
 
+	public static HashMap<Long, Integer> anagramMap = new HashMap<Long, Integer>();
+	public static HashMap<Character, Integer> valueMap = new HashMap<Character, Integer>();
+	
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		setUpValueMap();
 		// TODO Auto-generated method stub
 
 		// Multiply all of the letters' values together,
 		// ignoring spaces, creating a key for each word 
 		// to compare with. Add the key with a counter to 
 		// the HashMap, and add it to the key array.
+
+		/////////////////////////////////////////
+		// Reading the files
+		/////////////////////////////////////////
+
+		// members
+		FileReaderBasic basicFileReader = new FileReaderBasic();
+		
+		System.out.println("Lecture de la liste de mot en cours");
+		ArrayList<String> listDictionary = (ArrayList<String>) basicFileReader.readDictionary(args[0]);
+		System.out.println("Lecture du dictionnaire en cours");
+		ArrayList<String> listLines = (ArrayList<String>) basicFileReader.readLines(args[1]);
+		System.out.println("Chargement des fichiers terminés");
+		setUpAnagramMap(listDictionary);
+		/////////////////////////////////////////
+		// Algorithm execution
+		/////////////////////////////////////////
+		
+		// Calculate the time for the algorithm.
+		//Deuxi�me algorithme.
+		//stopwatch clear & start.
+		long initialTime = 0;
+		long finalTime = 0;
+		
+		initialTime = System.nanoTime();
+		for(int i = 0; i < listLines.size(); i++){
+			
+			//String stringToAnalyze = listDictionary.get(i).toString();
+			//String stringToAnalyze = listLines.get(i);
+			
+			//System.out.println("Analyse du mot " + stringToAnalyze);
+					//(String)listLines.toArray()[i];
+			EstUnAnagrammeEfficace(listLines.get(i));
+			
+		
+		}
+		finalTime = System.nanoTime();
+
+		//System.out.print("time of the second algorithm operation in nanoseconds is = ");
+		System.out.println(finalTime - initialTime);
 		
 		
+
+		for(int i = 0; i < listDictionary.size();++i){
+			long dictionaryKey = 1;
+			//String[] listeArray = Arrays.copyOf(listDictionary.toArray(), listDictionary.toArray().length, String[].class);
+			//char[] chaineArray = listeArray[i].toCharArray();
+			char[] chaineArray = listDictionary.get(i).toCharArray();
+
+			for(int j = 0; j < chaineArray.length; j++){
+
+				dictionaryKey = dictionaryKey * valueMap.get(chaineArray[j]);
+
+			}
+
+			//System.out.println("Il y a " + anagramMap.get(dictionaryKey) + " anagrammes du mot " + listeArray[i]);
+			System.out.println("Il y a " + anagramMap.get(dictionaryKey) + " anagrammes du mot " + listDictionary.get(i));
+
+		}
+		long endTime = System.nanoTime();
+		
+		System.out.println("The whole program took "+(endTime - startTime) + " ns"); 
 	}
 	
-	public boolean EstUnAnagrammeNaif(String chaine1, String chaine2){
+	public static boolean EstUnAnagrammeNaif(String chaine1, String chaine2){
 		boolean found = false;
 		StringBuilder chaine2StringBuild = new StringBuilder(chaine2);
-		
+		chaine1 = chaine1.replaceAll("\\s+","");
+		chaine2 = chaine2.replaceAll("\\s+","");
 		for(int i = 0; i < chaine1.length(); i++){
-			
+
 			found = false;
 			
 			for(int j = 0; j < chaine2.length(); j++){
-				
-				while(!found){
 					
-					if (chaine1.toCharArray()[i] == chaine2.toCharArray()[j]){
+				if (chaine1.toCharArray()[i] == chaine2.toCharArray()[j]){
 						
-						//Remove char from string
-						chaine2 = chaine2StringBuild.deleteCharAt(j).toString();
-						found = true;
-					}
-					
+					//Remove char from string
+					chaine2 = chaine2StringBuild.deleteCharAt(j).toString();
+					found = true;
+					break;
 				}
+					
 			}
 		}
 		
@@ -51,19 +118,91 @@ public class Main {
 		return true;
 	}
 	
-	public boolean EstUnAnagrammeEfficace(String chaine1, String chaine2){
+	public static void EstUnAnagrammeEfficace(String chaine){
 		
-		// Letter/Number Array goes here.
-		
+		long dictionaryKey = 1;
+		chaine = chaine.replaceAll("\\s+","");
+		char[] chaineArray = chaine.toCharArray();
+		int anagramCounter = 0;
+
 		// For each word to compare, multiply all of the 
 		// letters' values together, just like previously. 
 		// If the resulting key exists, add 1 to the associated 
 		// counter in the HashMap.
+		for(int i = 0; i < chaine.length(); i++){
+			
+			dictionaryKey = dictionaryKey * valueMap.get(chaineArray[i]);
+			
+		}
 		
 		// With the key array and the HashMap, you now have
-		// a list of how many anagrams exist for each word!
+		// a list of how many anagrams exist for each word.
+
+		try {
+			anagramCounter = anagramMap.get(dictionaryKey) + 1;
+		} catch(Exception e){
+			anagramCounter = 0;
+		}
+		anagramMap.put(dictionaryKey, anagramCounter);
+	}
+	
+	public static void setUpValueMap(){
+		valueMap.put('a', 2);
+		valueMap.put('b', 3);
+		valueMap.put('c', 5);
+		valueMap.put('d', 7);
+		valueMap.put('e', 11);
+		valueMap.put('f', 13);
+		valueMap.put('g', 17);
+		valueMap.put('h', 19);
+		valueMap.put('i', 23);
+		valueMap.put('j', 29);
+		valueMap.put('k', 31);
+		valueMap.put('l', 37);
+		valueMap.put('m', 41);
+		valueMap.put('n', 43);
+		valueMap.put('o', 47);
+		valueMap.put('p', 53);
+		valueMap.put('q', 59);
+		valueMap.put('r', 61);
+		valueMap.put('s', 67);
+		valueMap.put('t', 71);
+		valueMap.put('u', 73);
+		valueMap.put('v', 79);
+		valueMap.put('w', 83);
+		valueMap.put('x', 89);
+		valueMap.put('y', 97);
+		valueMap.put('z', 101);
+		valueMap.put('0', 103);
+		valueMap.put('1', 107);
+		valueMap.put('2', 109);
+		valueMap.put('3', 113);
+		valueMap.put('4', 127);
+		valueMap.put('5', 131);
+		valueMap.put('6', 137);
+		valueMap.put('7', 139);
+		valueMap.put('8', 149);
+		valueMap.put('9', 151);
+		valueMap.put(' ', 1);
+	}
+	
+	public static void setUpAnagramMap(ArrayList<String> listDictionary){
 		
-		return true;
+		String[] dictionaryArray = Arrays.copyOf(listDictionary.toArray(), listDictionary.toArray().length, String[].class);
+		long dictionaryKey = 1;
+		
+		for(int i = 0; i < dictionaryArray.length; i++){
+
+			for(int j = 0; j < dictionaryArray[i].length(); j++){
+				
+				dictionaryKey = dictionaryKey * valueMap.get(dictionaryArray[i].charAt(j));
+				
+			}
+			
+			anagramMap.put(dictionaryKey, 0);
+			dictionaryKey = 1;
+		}
+		
 	}
 
 }
